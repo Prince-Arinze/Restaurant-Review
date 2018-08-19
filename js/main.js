3cf431c5ec1d611d0cedb4fd6aba5cdc484bd1a0
Prince-ndu <prynzdacypher@gmail.com>
2018-08-19 02:09:04 +0100

    Modified main.js
    implemented the correct semantic and also corrected the img alt attribute text to be displayed

--- js/main.js
+++ js/main.js
@@ -161,9 +161,10 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.alt = `Image of ${restaurant.name}`;
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   li.append(name);
 
